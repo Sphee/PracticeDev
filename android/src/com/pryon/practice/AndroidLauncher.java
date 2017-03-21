f2aa82864de536c5cb3fbb30e72f41bca2d3f77a
Sphee <aratomix@gmail.com>
2017-03-20 19:53:47 -0500

    Beginning Start-Up
    Beginning gamedev, very basic practice

---
+++ android/src/com/pryon/practice/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pryon.practice;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pryon.practice.Practice;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Practice(), config);
+	}
+}
---
+++ html/src/com/pryon/practice/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pryon.practice.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pryon.practice.Practice;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Practice();
+        }
+}
